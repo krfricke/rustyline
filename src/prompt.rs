985d09fb16755d892aa3bd417f8f4350ce0e29a4
Kai Fricke <kai.fricke@openai.com>
2025-05-08 22:42:59 +0200

    remove update_prompt
    Signed-off-by: Kai Fricke <kai.fricke@openai.com>

--- src/prompt.rs
+++ src/prompt.rs
@@ -27,29 +27,3 @@ impl Prompt {
         self.0.load().clone()
     }
 }
-
-/// Update prompt with last unfinished line of the input
-pub fn update_prompt(prompt: &Prompt, line: String) -> String {
-    let mut lines = line.lines();
-    let last_line = match lines.next_back() {
-        Some(line) => line,
-        None => {
-            prompt.set_prompt("".to_string());
-            return String::new();
-        }
-    }
-    .to_string();
-
-    // get first lines
-    let mut first_lines = String::new();
-
-    let previous_prompt = prompt.swap(last_line.clone());
-    first_lines.push_str(previous_prompt.as_ref());
-
-    for line in lines {
-        first_lines.push_str(line);
-        first_lines.push('\n');
-    }
-
-    first_lines
-}
